8938578c346bfc33c9e9655113cad7f37c35b1d0
Stan-breaks <stanleymwendwa03@gmail.com>
2024-08-27 23:05:57 +0300

    scanner parenthesis

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	LEFT_PAREN  rune = '('
+	RIGHT_PAREN rune = ')'
+)
+
+func main() {
+	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+
+	if command != "tokenize" {
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
+		os.Exit(1)
+	}
+
+	filename := os.Args[2]
+	rawFileContents, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fileContents := string(rawFileContents)
+	for _, item := range fileContents {
+		switch item {
+		case LEFT_PAREN:
+			fmt.Println("LEFT_PAREN ( null")
+		case RIGHT_PAREN:
+			fmt.Println("RIGHT_PAREN ) null")
+		}
+	}
+	fmt.Println("EOF  null")
+}
