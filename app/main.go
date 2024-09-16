636a9dbc5dc63e038bc46c7a8835a0649fe34e6e
Stan-breaks <stanleymwendwa03@gmail.com>
2024-09-16 15:38:49 +0300

    refactor

---
+++ app/main.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"fmt"
+	"github.com/Stan-breaks/app/tokenize"
+	"os"
+)
+
+func main() {
+	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+
+	if command != "tokenize" {
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
+		os.Exit(1)
+	}
+
+	filename := os.Args[2]
+	rawFileContents, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+	fileContents := string(rawFileContents)
+	fileLenght := len(fileContents) - 1
+	tokenize.Tokenize(fileContents, fileLenght)
+}
