b929691c59fa6824b7c447f69bfb91e711620881
Stan-breaks <stanleymwendwa03@gmail.com>
2024-09-16 15:38:40 +0300

    runes

---
+++ app/utils/runes.go
@@ -0,0 +1,20 @@
+package utils
+
+const (
+	LEFT_PAREN  rune = '('
+	RIGHT_PAREN rune = ')'
+	LEFT_BRACE  rune = '{'
+	RIGHT_BRACE rune = '}'
+	STAR        rune = '*'
+	DOT         rune = '.'
+	COMMA       rune = ','
+	PLUS        rune = '+'
+	MINUS       rune = '-'
+	SEMICOLON   rune = ';'
+	EQUAL       rune = '='
+	BANG        rune = '!'
+	LESS        rune = '<'
+	GREATER     rune = '>'
+	SLASH       rune = '/'
+	QUOTE       rune = '"'
+)
