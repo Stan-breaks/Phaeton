ff176c5d65209acc0832f2f7aafe347a6a39e76c
Stan-breaks <stanleymwendwa03@gmail.com>
2024-10-25 05:15:53 +0300

    parse parrathesis

--- app/parse/parser.go
+++ app/parse/parser.go
@@ -1,6 +1,7 @@
 package parse
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -11,7 +12,7 @@ func Parse(tokens models.Tokens) models.Node {
 	var left, right models.Node
 	op := ""
 	numToken := len(tokens.Success)
-	if numToken == 3 {
+	if numToken == 3 && strings.Split(tokens.Success[0], " ")[0] == "NUMBER" {
 		for index, item := range tokens.Success {
 			splitToken := strings.Split(item, " ")
 			switch index {
@@ -28,16 +29,14 @@ func Parse(tokens models.Tokens) models.Node {
 			Op:    op,
 			Right: right,
 		}
-
+	} else {
+		if numToken == 1 {
+			splitToken := strings.Split(tokens.Success[0], " ")
+			return parsevalue(splitToken)
+		}
+		return parseParrenthesisExpr(tokens.Success)
 	}
-	if numToken == 1 {
-		splitToken := strings.Split(tokens.Success[0], " ")
-		return parsevalue(splitToken)
-	}
-
-	return nil
 }
-
 func parseOperator(splitToken []string) string {
 	switch splitToken[0] {
 	case "PLUS", "MINUS", "STAR", "SLASH", "EQUAL_EQUAL", "LESS", "AND", "OR":
@@ -65,4 +64,28 @@ func parsevalue(splitToken []string) models.Node {
 	default:
 		return nil
 	}
+}
+
+func parseParrenthesisExpr(tokens []string) models.Node {
+	var value models.Node
+	paren := 0
+	for _, item := range tokens {
+		splitToken := strings.Split(item, " ")
+		switch splitToken[0] {
+		case "LEFT_PAREN":
+			paren = 1
+		case "RIGHT_PAREN":
+			if paren == 0 {
+				return models.NilNode{}
+			}
+		case "STRING", "NUMBER", "TRUE", "FALSE":
+			value = parsevalue(splitToken)
+		default:
+			return models.NilNode{}
+		}
+	}
+	return models.StringNode{
+		Value: "(group " + value.String() + ")",
+	}
+
 }
