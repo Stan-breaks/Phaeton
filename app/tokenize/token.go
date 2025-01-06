90852314a96f552cabb22aed40fbe845a1092541
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-07 00:28:52 +0300

    tokenizer
    formatting

--- app/tokenize/token.go
+++ app/tokenize/token.go
@@ -202,14 +202,26 @@ func Tokenize(fileContents string, fileLenght int) models.Tokens {
 					skipCount = 0
 				} else {
 					if i == fileLenght {
-						tokens.Success = append(tokens.Success, "LESS < null")
+						token := models.TokenInfo{
+							Token: "LESS < null",
+							Line:  line,
+						}
+						tokens.Success = append(tokens.Success, token)
 					} else {
 						switch fileContents[i+1] {
 						case byte(utils.EQUAL):
-							tokens.Success = append(tokens.Success, "LESS_EQUAL <= null")
+							token := models.TokenInfo{
+								Token: "LESS_EQUAL <= null",
+								Line:  line,
+							}
+							tokens.Success = append(tokens.Success, token)
 							skipCount = 1
 						default:
-							tokens.Success = append(tokens.Success, "LESS < null")
+							token := models.TokenInfo{
+								Token: "LESS < null",
+								Line:  line,
+							}
+							tokens.Success = append(tokens.Success, token)
 						}
 					}
 				}
