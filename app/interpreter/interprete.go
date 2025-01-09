f91fa9f116a3d5bb8ebf1a6ec2403b0385248b78
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-09 15:27:55 +0300

    interpreter
    handling core logic for print and if

--- app/interpreter/interprete.go
+++ app/interpreter/interprete.go
@@ -2,87 +2,129 @@ package interpreter
 
 import (
 	"fmt"
-	"strings"
-
 	"github.com/Stan-breaks/app/models"
 	"github.com/Stan-breaks/app/parse"
+	"strings"
 )
 
 func Interprete(tokens []models.TokenInfo) error {
 	currentPosition := 0
 	for currentPosition < len(tokens) {
-		if strings.HasPrefix(tokens[currentPosition].Token, "IF") {
-			ifLine := tokens[currentPosition].Line
+		if currentPosition >= len(tokens) {
+			break
+		}
+		token := tokens[currentPosition]
+		switch {
+		case strings.HasPrefix(token.Token, "IF"):
+			ifLine := token.Line
 			end := findMatchingEnd(ifLine, currentPosition, tokens)
 			err := handleIf(tokens[currentPosition:end])
 			if err != nil {
-				return err
+				return fmt.Errorf("line %d: %v", ifLine, err)
 			}
 			currentPosition = end
-		} else if strings.HasPrefix(tokens[currentPosition].Token, "PRINT") {
-			splitToken := strings.Split(tokens[currentPosition+1].Token, " ")
-			fmt.Print(splitToken[2])
-			currentPosition += 2
-		} else {
+
+		case strings.HasPrefix(token.Token, "PRINT"):
+			end, err := handlePrint(tokens[currentPosition:])
+			if err != nil {
+				return err
+			}
+			currentPosition += end
+
+		default:
 			currentPosition++
 		}
 	}
 	return nil
 }
 
-func findMatchingEnd(initialLine int, currentPosition int, tokens []models.TokenInfo) int {
-	endToken := 0
-	for i := currentPosition; i < len(tokens); i++ {
+func handlePrint(tokens []models.TokenInfo) (int, error) {
+	if len(tokens) < 2 {
+		return 0, fmt.Errorf("incomplete print statement")
+	}
+	expr, err := parse.Parse(tokens[1:])
+	if err != nil {
+		return 0, fmt.Errorf("invalid print expression")
+	}
+	result := expr.Evaluate()
+	fmt.Print(result)
+	tokensUsed := 2
+	for i := 1; i < len(tokens); i++ {
 		if strings.HasPrefix(tokens[i].Token, "SEMICOLON") {
-			if initialLine == tokens[i].Line {
-				endToken = i
-			} else {
-				endToken = i + 1
-			}
-			return endToken
+			tokensUsed = i + 1
+			break
 		}
 	}
-	return endToken
+
+	return tokensUsed, nil
+}
+
+func findMatchingEnd(initialLine int, currentPosition int, tokens []models.TokenInfo) int {
+	braceCount := 0
+	for i := currentPosition; i < len(tokens); i++ {
+		token := tokens[i].Token
+		if strings.HasPrefix(token, "LEFT_BRACE") {
+			braceCount++
+		} else if strings.HasPrefix(token, "RIGHT_BRACE") {
+			braceCount--
+			if braceCount == 0 && initialLine != tokens[i].Line {
+				return i + 1
+			}
+		} else if strings.HasPrefix(token, "SEMICOLON") && braceCount == 0 {
+			if initialLine == tokens[i].Line {
+				return i + 1
+			}
+		}
+	}
+	return len(tokens)
 }
 
 func handleIf(tokens []models.TokenInfo) error {
-	currentPosition := 1
-	startCondition := 0
-	endCondition := 0
-	startBody := 0
-	endBody := 0
-	for i := currentPosition; i < len(tokens); i++ {
-		if strings.HasPrefix(tokens[i].Token, "LEFT_PAREN") {
-			startCondition = i
-		}
-		if strings.HasPrefix(tokens[i].Token, "RIGHT_PAREN") {
-			endCondition = i
-			if strings.HasPrefix(tokens[i+1].Token, "LEFT_BRACE") {
-				startBody = i + 2
-			} else {
-				startBody = i + 1
+	conditionStart := -1
+	conditionEnd := -1
+	bodyStart := -1
+	bodyEnd := -1
+	braceCount := 0
+
+	for i := 0; i < len(tokens); i++ {
+		token := tokens[i].Token
+
+		switch {
+		case strings.HasPrefix(token, "LEFT_PAREN") && conditionStart == -1:
+			conditionStart = i
+		case strings.HasPrefix(token, "RIGHT_PAREN"):
+			conditionEnd = i
+		case strings.HasPrefix(token, "LEFT_BRACE"):
+			if bodyStart == -1 {
+				bodyStart = i + 1
+			}
+			braceCount++
+		case strings.HasPrefix(token, "RIGHT_BRACE"):
+			braceCount--
+			if braceCount == 0 {
+				bodyEnd = i - 1
 			}
 		}
-		if strings.HasPrefix(tokens[i].Token, "RIGHT_BRACE") {
-			endBody = i
-		}
 	}
-	if endBody == 0 {
-		endBody = len(tokens) - 1
+
+	if bodyStart == -1 && conditionEnd != -1 {
+		bodyStart = conditionEnd + 1
+		bodyEnd = len(tokens) - 1
 	}
-	if startCondition != 0 && endCondition != 0 && startBody != 0 && endBody != 0 {
-		return fmt.Errorf("invalid if")
-	} else {
-		condition, err := parse.Parse(tokens[startCondition:endCondition])
-		if err != nil {
-			return fmt.Errorf("invalid if condition")
-		}
-		if condition.Evaluate().(bool) {
-			err := Interprete(tokens[startBody:endBody])
-			if err != nil {
-				return fmt.Errorf("invalid if body")
-			}
-		}
-		return nil
+	if conditionStart == -1 || conditionEnd == -1 || bodyStart == -1 {
+		return fmt.Errorf("malformed if statement")
 	}
+	condition, err := parse.Parse(tokens[conditionStart : conditionEnd+1])
+	if err != nil {
+		return fmt.Errorf("invalid condition: %v", err)
+	}
+
+	if condition.Evaluate().(bool) {
+		if bodyEnd == -1 {
+			bodyEnd = len(tokens)
+		}
+		return Interprete(tokens[bodyStart:bodyEnd])
+	}
+
+	return nil
 }
