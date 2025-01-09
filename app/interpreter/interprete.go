08104189621bdc7809ed8943eb85d6b5ae02b6b0
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-09 14:14:01 +0300

    interpreter
    handling if init

--- app/interpreter/interprete.go
+++ app/interpreter/interprete.go
@@ -1,10 +1,41 @@
 package interpreter
 
-import "github.com/Stan-breaks/app/models"
+import (
+	"fmt"
+	"strings"
 
-func Interprete(tokens models.Tokens) {
-	for token :=  tokens.Success {
+	"github.com/Stan-breaks/app/models"
+)
 
+func Interprete(tokens []models.TokenInfo) {
+	currentPosition := 0
+	for currentPosition < len(tokens) {
+		if strings.HasPrefix(tokens[currentPosition].Token, "IF") {
+			ifLine := tokens[currentPosition].Line
+			end := findMatchingEnd(ifLine, currentPosition, tokens)
+			handleIf(tokens[currentPosition:end])
+			currentPosition = end
+		} else {
+			currentPosition++
+		}
 	}
+}
+
+func findMatchingEnd(initialLine int, currentPosition int, tokens []models.TokenInfo) int {
+	endToken := 0
+	for i := currentPosition; i < len(tokens); i++ {
+		if strings.HasPrefix(tokens[i].Token, "SEMICOLON") {
+			if initialLine == tokens[i].Line {
+				endToken = i
+			} else {
+				endToken = i + 1
+			}
+			return endToken
+		}
+	}
+	return endToken
+}
+
+func handleIf(tokens []models.TokenInfo) error {
 
 }
