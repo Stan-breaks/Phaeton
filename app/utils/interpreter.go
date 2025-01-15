bfb4ac33d62b0313d06abed7b595fe759c2d908b
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-15 15:29:34 +0300

    interpreter
    bound error check for checking for reassignment

--- app/utils/interpreter.go
+++ app/utils/interpreter.go
@@ -7,5 +7,8 @@ import (
 )
 
 func IsReassignment(tokens []models.TokenInfo) bool {
+	if len(tokens) < 2 {
+		return false
+	}
 	return strings.HasPrefix(tokens[0].Token, "IDENTIFIER") && strings.HasPrefix(tokens[1].Token, "EQUAL")
 }
