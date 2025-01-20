c6896d1b7c6920acd68d121e503ca9c1b7838495
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-20 16:05:41 +0300

    refractoring
    I used AI to refactor my code into a more readable and maintaible
    it lookes more neat
    intrepreter refactoring

--- app/utils/interpreter.go
+++ app/utils/interpreter.go
@@ -12,3 +12,12 @@ func IsReassignmentCondition(tokens []models.TokenInfo) bool {
 	}
 	return strings.HasPrefix(tokens[0].Token, "IDENTIFIER") && strings.HasPrefix(tokens[1].Token, "EQUAL")
 }
+
+func FindSemicolonPosition(tokens []models.TokenInfo) int {
+	for i := 0; i < len(tokens); i++ {
+		if strings.HasPrefix(tokens[i].Token, "SEMICOLON") {
+			return i
+		}
+	}
+	return -1
+}
