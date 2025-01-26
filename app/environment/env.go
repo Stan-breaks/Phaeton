4451528c431b63c963597fe72d6eadce81d3aa72
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-26 22:32:05 +0300

    environment
    adding scoping to variables

--- app/environment/env.go
+++ app/environment/env.go
@@ -1,3 +1,35 @@
 package environment
 
-var Environment = make(map[string]interface{})
+type Environment struct {
+	Scope []map[string]interface{}
+}
+
+var Global = &Environment{
+	Scope: []map[string]interface{}{
+		make(map[string]interface{}),
+	},
+}
+
+func (e *Environment) PushScope() {
+	e.Scope = append(e.Scope, make(map[string]interface{}))
+}
+
+func (e *Environment) PopScope() {
+	if len(e.Scope) > 1 {
+		e.Scope = e.Scope[:len(e.Scope)-1]
+	}
+}
+
+func (e *Environment) Set(variableName string, value interface{}) {
+	e.Scope[len(e.Scope)-1][variableName] = value
+}
+
+func (e *Environment) Get(variableName string) (interface{}, bool) {
+	for i := len(e.Scope) - 1; i >= 0; i-- {
+		if val, ok := e.Scope[i][variableName]; ok {
+			return val, true
+		}
+
+	}
+	return nil, false
+}
