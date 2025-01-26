4ccbd52028067648fea55575b858a0b2f37957e6
Stan-breaks <stanleymwendwa03@gmail.com>
2025-01-26 09:25:43 +0300

    models
    adding while statements positions struct

--- app/models/interpreter.go
+++ app/models/interpreter.go
@@ -25,3 +25,14 @@ func (p IfStatementPositions) IsValid() bool {
 func (p IfStatementPositions) HasElseBlock() bool {
 	return p.ElseBodyEnd != -1 && p.ElseBodyStart != -1
 }
+
+type WhileStatementPositions struct {
+	ConditionStart int
+	ConditionEnd   int
+	BodyStart      int
+	BodyEnd        int
+}
+
+func (p WhileStatementPositions) IsValid() bool {
+	return p.ConditionStart != -1 && p.ConditionEnd != -1 && p.BodyStart != -1 && p.BodyEnd != -1
+}
