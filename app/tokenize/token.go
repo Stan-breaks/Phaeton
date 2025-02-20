f3ea0b193f822f11f75781e5103b3b998b38241b
Stan-breaks <stanleymwendwa03@gmail.com>
2025-02-20 14:29:28 +0300

    tokenize
    properly tokenized equal and equal equal

--- app/tokenize/token.go
+++ app/tokenize/token.go
@@ -417,24 +417,30 @@ func Tokenize(fileContents string, fileLenght int) models.Tokens {
 					skipCount = 0
 				} else {
 					if i == fileLenght {
-						token := models.TokenInfo{
-							Token: "EQUAL = null",
-							Line:  line,
+						token := models.Token{
+							Type:    models.EQUAL,
+							Lexem:   "=",
+							Literal: nil,
+							Line:    line,
 						}
 						tokens.Success = append(tokens.Success, token)
 					} else {
 						switch fileContents[i+1] {
 						case byte(runes.EQUAL):
-							token := models.TokenInfo{
-								Token: "EQUAL_EQUAL == null",
-								Line:  line,
+							token := models.Token{
+								Type:    models.EQUAL_EQUAL,
+								Lexem:   "==",
+								Literal: nil,
+								Line:    line,
 							}
 							tokens.Success = append(tokens.Success, token)
 							skipCount = 1
 						default:
-							token := models.TokenInfo{
-								Token: "EQUAL = null",
-								Line:  line,
+							token := models.Token{
+								Type:    models.EQUAL,
+								Lexem:   "=",
+								Literal: nil,
+								Line:    line,
 							}
 							tokens.Success = append(tokens.Success, token)
 						}
